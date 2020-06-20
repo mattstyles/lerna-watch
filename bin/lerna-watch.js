94adb1bc81e8006c18a4246637852fe4d0facf80
Matt Styles <matt@personalurban.com>
2020-06-20 10:12:06 +0100

    add evaluate local dependency graph

--- bin/lerna-watch.js
+++ bin/lerna-watch.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+const argv = require('minimist')(process.argv.slice(2))
+const { watch } = require('../lib')
+
+const targetName = argv._[0]
+
+try {
+  watch({
+    cwd: process.cwd(),
+    target: targetName
+  })
+} catch (err) {
+  // suppress errors in cli mode
+}
