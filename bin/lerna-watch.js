f55c7426b5feafd0e6981bf4f4d96eaf4fa00607
Matt Styles <matt@personalurban.com>
2020-06-30 16:27:48 +0100

    add help and version information to cli

--- bin/lerna-watch.js
+++ bin/lerna-watch.js
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
 
-const argv = require('minimist')(process.argv.slice(2))
+const argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    v: 'version',
+    h: 'help'
+  }
+})
 const { watch } = require('../lib')
+const pkg = require('../package.json')
+const man = require('../lib/man')
 
 const targetName = argv._[0]
+
+if (argv.version) {
+  console.log(`${pkg.version}`)
+  process.exit(0)
+}
+
+if (argv.help) {
+  console.log(man)
+  process.exit(0)
+}
 
 try {
   watch({
